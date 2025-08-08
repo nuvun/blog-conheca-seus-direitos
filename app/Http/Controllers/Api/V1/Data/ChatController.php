2fbb0fe0ee56274ebe62febe03792cb3dc72fdde
Italo Veloso <italoplus@gmail.com>
2025-08-08 19:06:28 -0300

    Adiciona rota e controlador para recuperação de dados dos chats

---
+++ app/Http/Controllers/Api/V1/Data/ChatController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Data;
+
+use App\Http\Controllers\Controller;
+use App\Models\ChatUserData;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+
+class ChatController extends Controller
+{
+
+    public function index(): JsonResponse
+    {
+        try {
+            $chatUserData = ChatUserData::query()
+                ->with(['chatMessages'])
+                ->orderByDesc('id')
+                ->get();
+
+            return response()->json([
+                'message' => 'Chat user data retrieved successfully.',
+                'data' => $chatUserData,
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage(),
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -15,3 +15,13 @@ Route::group(['prefix' => 'v1/auth', 'as' => 'api.auth.', 'namespace' => 'Api\V1
     });
 
 });
+
+Route::group(['prefix' => 'v1/data', 'as' => 'api.data.', 'namespace' => 'Api\V1\Data'], function() {
+
+    Route::controller(\App\Http\Controllers\Api\V1\Data\ChatController::class)->group(function () {
+        Route::get('/chat', 'index')
+            ->middleware(['auth:sanctum'])
+            ->name('chat.index');
+    });
+
+});
